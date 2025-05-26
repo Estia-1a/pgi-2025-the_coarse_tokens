ef6733a198532663cd567907d9a89cdd82d2e1e0
Juju-333 <julien.vigier@etu-estia.fr>
2025-05-26 12:30:11 +0200

    ajout first_pixel

--- src/features.c
+++ src/features.c
@@ -28,3 +28,22 @@ void dimension (char *source_path) {
      printf("erreur: ");
     }
 }
+void first_pixel(char *source_path) {
+    int width, height, channels;
+    unsigned char *data ;
+
+    int resultat1 = read_image_data (source_path, &data, &width, &height, &channels);
+    if (resultat1) { 
+    printf("Image chargée : %d x %d, %d canaux\n", width, height, channels);
+    unsigned char r = data[0];
+    unsigned char g = data[1];
+    unsigned char b = data[2];
+    printf("Couleur du premier pixel :\n");
+    printf("R = %d\n", r);
+    printf("G = %d\n", g);
+    printf("B = %d\n", b);
+    }
+    else {
+     printf("erreur: ");
+    }  
+}
--- src/features.h
+++ src/features.h
@@ -7,3 +7,5 @@ void helloWorld();
 #endif
 
 void dimension (char *source_path);
+
+void first_pixel (char *source_path);
