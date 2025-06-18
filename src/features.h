7a615a29cb1941dd7ab53e8f90f799b6c9093c54
YoumnaW <youmna.wajou@estia.fr>
2025-06-18 13:32:03 +0200

    modification max_pixel

--- src/features.h
+++ src/features.h
@@ -12,4 +12,9 @@ void dimension (char *source_path);
 void first_pixel (char *source_path);
 
 void print_pixel (char *source_path);
+
 void second_line(char *source_path);
+
+void max_pixel(char *source_path); 
+
+void max_component(char *source_path, char component);
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdlib.h> 
+#include <stddef.h> 
+
 /**
  * @brief Here, you have to define pixelRGB struct and its functions:
  * - pixel* getPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y );
@@ -9,11 +12,12 @@
 */
 
 typedef struct _pixelRGB {
-  /*
-   * TO COMPLETE
-   */
-   char R ; 
-} pixelRGB ;
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+} pixelRGB;
 
 
-#endif
+pixelRGB* get_pixel(unsigned char* data, unsigned int width, unsigned int height, unsigned int n, unsigned int x, unsigned int y);
+
+#endif 
